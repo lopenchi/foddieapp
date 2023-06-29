7f98f7071a781db10a8fdd664eba82154c818931
Carolina López <clopez@stackbuilders.com>
2023-06-29 09:29:00 -0500

    Add tests for card component

--- tests/card.test.tsx
+++ tests/card.test.tsx
@@ -3,19 +3,23 @@ import {render, screen} from '@testing-library/react';
 import {Card} from "../src/ingredient/Card";
 
 describe("Accordion test", () => {
-    const ingredient = {
-        name: "tomato",
-        group: "vegetable",
-        minAge: 6
-    }
-
     test("should show ingredient name", () => {
-        render(<Card ingredient={ingredient} />);
+        render(<Card ingredient={{name: "tomato", group: "vegetable", minAge: 6}} />);
         expect(screen.getByText(/tomato/i)).toBeDefined();
     })
 
     test("should show border green for vegetables", () => {
-        render(<Card ingredient={ingredient} />);
+        render(<Card ingredient={{name: "tomato", group: "vegetable", minAge: 6}} />);
         expect(screen.getByRole("card").className).toContain("border-green-500");
+    });
+
+    test("should show border yellow for fruits", () => {
+        render(<Card ingredient={{name: "banana", group: "fruit", minAge: 6}} />);
+        expect(screen.getByRole("card").className).toContain("border-yellow-500");
+    });
+
+    test("should show border green for protein", () => {
+        render(<Card ingredient={{name: "chicken", group: "protein", minAge: 6}} />);
+        expect(screen.getByRole("card").className).toContain("border-red-500");
     });
 })
