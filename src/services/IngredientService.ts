cb21f1d57491b5cebc6d977183a7bdf6a4f6dca7
Carolina López <clopez@stackbuilders.com>
2023-07-21 10:09:40 -0500

    Adding routing on application (#5)
    * First attempt with routes
    
    * Routing (WIP)
    
    * Routing working
    
    * Active link in sidebar

--- src/services/IngredientService.ts
+++ src/services/IngredientService.ts
@@ -1,4 +1,4 @@
-import {collection, getDocs} from "@firebase/firestore"
+import {collection, doc, getDocs, getDoc} from "@firebase/firestore"
 import {database} from "../config/firebase.ts"
 
 const collectionRef = collection(database, "ingredients");
@@ -7,11 +7,29 @@ const getAllIngredients = (): Promise<Ingredient[]> => {
     return getDocs(collectionRef)
         .then((docData) => {
             return docData.docs
-                .map((doc) => ({...doc.data() as Ingredient}));
+                .map((doc) => ({...doc.data(), id: doc.id} as Ingredient));
         }).catch((err) => {
             console.log(err);
             return [];
         })
 }
 
-export {getAllIngredients}
+const getAllIngredientsName = (): Promise<List[]> => {
+    return getDocs(collectionRef)
+        .then((docData) => {
+            return docData.docs
+                .map((doc) => ({id: doc.id, name: doc.data().name} as List));
+        }).catch((err) => {
+            console.log(err);
+            return [];
+        })
+}
+
+const getIngredientById = async (id: string): Promise<Ingredient> => {
+    const docRef = doc(database, "ingredients", id);
+
+    const docSnap = await getDoc(docRef);
+    return docSnap.data() as Ingredient;
+}
+
+export {getAllIngredients, getAllIngredientsName, getIngredientById}
